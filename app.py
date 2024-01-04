cf0d34e3eae629283050734bb28fab088fc8bd0f
Vivek Suvarna <153171652+Viveks063@users.noreply.github.com>
2024-01-04 16:41:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import pyttsx3
 from googletrans import Translator
 from flask import Flask, render_template, request
 
+
+def reply_with_voice(u_input):
+    engine=pyttsx3.init()
+    engine.setProperty('rate',100)
+    engine.setProperty('language','es')
+    engine.say(u_input)
+    engine.runAndWait()
 translator=Translator()
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
@@ -14,8 +22,11 @@ def index():
             return "Chatbot: Goodbye!"
         else:
             translate = translator.translate(user_input, src='en', dest='es').text
-    
+            
+        
+    reply_with_voice(translate)
     return render_template('index.html',translate=translate)
-    
+
+
 if __name__ == '__main__':
     app.run(debug=True)
